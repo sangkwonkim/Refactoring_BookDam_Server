916eab80267f991c6af79d3428e472103424770c
sangkwonkim <sangkwon2406@naver.com>
2022-03-01 21:35:53 +0900

    login testcode done

--- test/spec.js
+++ test/spec.js
@@ -1,34 +1,58 @@
 const app = require('../app');
 const request = require('supertest');
-const models = require('../models/index');
+const models = require('../models');
 const should = require('should');
 const { User: UserModel, Article: ArticleModel, Follow: FollowModel } = require('../models');
 
 describe('POST /user/login', () => {
+  before(() => models.sequelize.sync({ force: true }));
+  before(() => UserModel.queryInterface.bulkInsert('Users', [{
+    userId: 'guest',
+    password: '$2b$10$RJq0gXxBHhLsRhMtI8U3p./kk.KPvdohoMx179N3HvbUaDpPbMi1.',
+    userNickName: 'sangkwon',
+    userImage: 'https://img.icons8.com/flat-round/512/000000/bird--v1.png',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }]));
   describe('성공 시', () => {
-    before(() => models.sequelize.sync({ force: true }));
-    before(() => UserModel.queryInterface.bulkInsert('Users', [{
-      userId: 'guest',
-      password: '$2b$10$RJq0gXxBHhLsRhMtI8U3p./kk.KPvdohoMx179N3HvbUaDpPbMi1.',
-      userNickName: 'sangkwon',
-      userImage: 'https://img.icons8.com/flat-round/512/000000/bird--v1.png',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]));
-    it('로그인 성공 시 응답 상태 코드는 200을 반환한다.', (done) => {
+    it('응답 상태 코드는 200을 반환한다.', (done) => {
       request(app)
         .post('/user/login')
         .send({ userInfo: { userId: 'guest', password: '1234' } })
         .expect(200, done);
     });
-    it('로그인 성공 시 성공 메세지를 반환한다.', (done) => {
+    it('성공 메세지를 반환한다.', (done) => {
       request(app)
         .post('/user/login')
         .send({ userInfo: { userId: 'guest', password: '1234' } })
         .end((err, res) => {
           res.body.should.have.property('message', 'success');
+          res.body.should.have.property('userInfo');
           done();
         });
+    });
+    it('응답에 쿠키를 담아 전송합니다.', (done) => {
+      request(app)
+        .post('/user/login')
+        .send({ userInfo: { userId: 'guest', password: '1234' } })
+        .end((err, res) => {
+          res.headers['set-cookie'][0].split(';')[0].split('=')[0].should.equal('jwt');
+          done();
+        });
+    });
+  });
+  describe('실패 시', () => {
+    it('회원가입을 하지 않은 유저일 경우 404를 전송한다.', (done) => {
+      request(app)
+        .post('/user/login')
+        .send({ userInfo: { userId: 'sangkwon', password: '1234' } })
+        .expect(404, done);
+    });
+    it('유저 정보가 정확하지 않을 경우 400을 리턴한다.', (done) => {
+      request(app)
+        .post('/user/login')
+        .send({ userInfo: { userId: 'guest'} })
+        .expect(400, done);
     });
   });
 });
